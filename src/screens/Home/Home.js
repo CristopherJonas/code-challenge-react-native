1f00b13011bc818e828736c6ab3f4fa5d1fb3a41
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2020-08-08 23:07:08 -0300

    home screen

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
-
-// import { Container } from './styles';
+import {View, Text, SafeAreaView, Image} from 'react-native';
+import ButtonLink from '../../components/ButtonLink/ButtonLink';
+import CurrentBalance from '../../components/CurrentBalance/CurrentBalance';
+import NameLogo from '../../components/NameLogo/NameLogo';
+import * as S from './styles';
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>
-          tsadasdasdasdd
-      </Text>
-      </View>
-    </SafeAreaView>
-  )
-}
+    <S.HomeScreenWrapper>
+      <SafeAreaView>
+        <NameLogo />
+        <CurrentBalance balance={'19,90'} />
+        <S.ButtonsWrapper>
+          <ButtonLink
+            icon={'list'}
+            text={'Mostrar lista'}
+            navigateTo={'Transactions'}
+          />
+          <ButtonLink
+            icon={'plus'}
+            text={'Nova operação'}
+            navigateTo={'NewTransaction'}
+          />
+        </S.ButtonsWrapper>
+      </SafeAreaView>
+    </S.HomeScreenWrapper>
+  );
+};
 
 export default Home;
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const HomeScreenWrapper = styled.View`
+  background-color: #fff;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonsWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 25px 0 25px 0;
+`;
