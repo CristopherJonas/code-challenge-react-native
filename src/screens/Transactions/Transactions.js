15e9d41232f6db0f796833cbf192e7c2b43bef52
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2020-08-09 12:06:55 -0300

    transactions screen

--- src/screens/Transactions/Transactions.js
+++ src/screens/Transactions/Transactions.js
@@ -1,18 +1,64 @@
 import React from 'react';
-import { View,Text,SafeAreaView } from 'react-native';
+import {View, Text, FlatList} from 'react-native';
+import TransactionCard from '../../components/TransactionCard/TransactionCard';
 
-// import { Container } from './styles';
+import * as S from './styles';
+
+const data = [
+  {
+    id: 1,
+    type: 'increment',
+    value: 1985645615.55,
+    description: 'transação sobre uhau asdasdioj açsdlasdç',
+  },
+  {
+    id: 2,
+    type: 'increment',
+    value: 19856615.55,
+    description: 'transação sobre uhau asdasdioj ',
+  },
+  {
+    id: 3,
+    type: 'decrement',
+    value: 198615.55,
+    description: 'transação sobre uhau ',
+  },
+  {
+    id: 4,
+    type: 'increment',
+    value: 19815.55,
+    description: 'transação sobre ',
+  },
+  {
+    id: 5,
+    type: 'decrement',
+    value: 115.55,
+    description: 'transação ',
+  },
+  {
+    id: 6,
+    type: 'decrement',
+    value: 14.57,
+    description: 'transação sobre uhau asdasdioj açsdlasdç',
+  },
+];
 
 const Transactions = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>
-          Transactions
-      </Text>
-      </View>
-    </SafeAreaView>
-  )
-}
+    <S.TransactionsScreenWrapper>
+      <FlatList
+        data={data}
+        keyExtractor={(transaction) => transaction.id.toString()}
+        renderItem={({item}) => (
+          <TransactionCard
+            type={item.type}
+            description={item.description}
+            value={item.value}
+          />
+        )}
+      />
+    </S.TransactionsScreenWrapper>
+  );
+};
 
 export default Transactions;
---
+++ src/screens/Transactions/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const TransactionsScreenWrapper = styled.View`
+  background-color: #fff;
+  flex: 1;
+  align-items: center;
+  padding: 10px 10px 0 10px;
+`;
