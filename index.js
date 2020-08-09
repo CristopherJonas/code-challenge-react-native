5efad4eb2e42a57b3bdb0c7a9a8e301dc421869d
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2020-08-09 11:55:20 -0300

    refactor: put load icon in index

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler';
 import App from './src/App';
 import {name as appName} from './app.json';
+import Icon from 'react-native-vector-icons/FontAwesome';
+Icon.loadFont();
 
 AppRegistry.registerComponent(appName, () => App);
