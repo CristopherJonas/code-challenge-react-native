25dabb0bdcd0aef13a428f375a6e33a8daf02c51
Cristopher Jonas de Faria <cristopherjonasfaria@gmail.com>
2020-08-09 23:05:28 -0300

    create fake Database

---
+++ src/database/database.js
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem('transactions', jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+export const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('transactions');
+    return jsonValue != null ? JSON.parse(jsonValue) : [];
+  } catch (e) {
+    // error reading value
+  }
+};
+
+export const removeValue = async () => {
+  try {
+    await AsyncStorage.removeItem('transactions');
+  } catch (e) {
+    // remove error
+  }
+  console.log('Done.');
+};
